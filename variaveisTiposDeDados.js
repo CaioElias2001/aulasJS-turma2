c52895365586b22a29777ab453ee9913281526dd
CaioElias2001 <89169367+CaioElias2001@users.noreply.github.com>
2023-07-04 14:50:03 -0300

    Add files via upload
    Tipos de dados em variáveis

---
+++ variaveisTiposDeDados.js
@@ -0,0 +1,44 @@
+var num1 = 2;
+console.log(num1);
+
+var num2 = 4;
+console.log(num2);
+
+var nome1 = "Caio";
+console.log(nome1);
+
+var nome2 = "Alef";
+console.log(nome2);
+
+var v = true;
+console.log("É verdade?: " + v);
+
+var m = false;
+console.log("É falso?: " + m);
+
+var lista1 = ["Caio", "Bispo", "Elias"];
+console.log(lista1);
+
+var lista2 = [1, 2, 3];
+console.log(lista2);
+
+var objeto1 = {obj1: 1, obj2: 2, obj3: 3};
+console.log(objeto1);
+console.log(objeto1.obj1);
+console.log(objeto1.obj2);
+console.log(objeto1.obj3);
+
+var objeto2 = {obj1: "Caio", obj2: "Bispo", obj3: "Elias"};
+console.log(objeto2);
+console.log(objeto2.obj1);
+console.log(objeto2.obj2);
+console.log(objeto2.obj3);
+
+var v1;
+var v2;
+
+var zero1 = 0;
+console.log(zero1);
+
+var zero2 = 0;
+console.log(zero2);
