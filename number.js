6750fc4949ac7e1e45614cc2783d3e7b7bd6dfa5
CaioElias2001 <89169367+CaioElias2001@users.noreply.github.com>
2023-07-04 18:53:08 -0300

    Add files via upload
    Objeto Number

---
+++ number.js
@@ -0,0 +1,23 @@
+//1) Criar 3 variáveis com tipos Objeto Number para guardar os valores:
+//  a) 300, 999 e 21.
+//  b) Exibir no console os valores das variáveis.
+var n1 = new Number(300)
+console.log(n1)
+var n2 = new Number(999)
+console.log(n2)
+var n3 = new Number(21)
+console.log(n3)
+
+//2) Exibir no console os valores de 4 propriedades do tipo objeto Number.
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
+//3) Criar 1 variável para receber o valor 150, em seguida faça:
+//  a) Exibir no console o valor da variável.
+//  b) Converter para string o valor.
+//  c) Exibir no console, o valor convertido.
+var n4 = new Number(150)
+console.log(n4)
+console.log(n4.toString())
