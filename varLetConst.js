53db38c281df2467e5e7cc4d7edc3f6e28b210e8
CaioElias2001 <89169367+CaioElias2001@users.noreply.github.com>
2023-07-04 14:48:14 -0300

    Add files via upload
    Variáveis e Constantes

---
+++ varLetConst.js
@@ -0,0 +1,22 @@
+var num1 = 2
+console.log(num1)
+num1 = 3
+console.log(num1)
+var nome1 = "Caio"
+console.log(nome1)
+nome1 = "Bispo"
+console.log(nome1)
+
+let num2 = 3
+console.log(num2)
+num2 = 2
+console.log(num2)
+let nome2 = "Alef"
+console.log(nome2)
+nome2 = "Santos"
+console.log(nome2)
+
+const num3 = 4
+console.log(num3)
+const nome3 = "Elias"
+console.log(nome3)
